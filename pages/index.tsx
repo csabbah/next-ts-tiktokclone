14b567ad6a0c24c8295b4bbde7abf696b0835d7c
csabbah <csabbah@ryerson.ca>
2023-02-02 18:41:54 -0500

    installed and setup tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>TikTok</div>;
+  return <div className="text-3xl font-bold underline">TikTok</div>;
 };
 export default Home;

0a5e775b298f128cbec97e750eba552a4a5e9fcf
csabbah <csabbah@ryerson.ca>
2023-02-02 19:46:24 -0500

    Added new utils folder with a few files including website logo & created new Navbar & Sidebar components. Furthermore, populated Navbar with initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
@@ -16,10 +18,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div>
-      NavBar
+      <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          Sidebar
+          <Sidebar />
         </div>
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
           <Component {...pageProps} />
